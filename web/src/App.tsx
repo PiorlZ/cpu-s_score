ede5bcb7c9b795c039fbe3a4392453a47da646c4
merousss <tim_5678@mail.ru>
2024-10-21 09:21:00 +0300

    Create React app

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { ProcTable } from './components/ProcTable';
+import { useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/components/ProcTable.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Table, Form } from "rsuite";
+
+
+export const ProcTable = ({tableData}) => {
+
+
+  return(
+    <Table
+      shouldUpdateScroll={false}
+      height={850}
+    >
+      
+    </Table>
+  );
+}
